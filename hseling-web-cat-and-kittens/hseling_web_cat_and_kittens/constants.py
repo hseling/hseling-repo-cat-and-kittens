e59ffb02e3fd00fbad44f83302cbf2b3117a320a
ElizavetaNosova <58087973+ElizavetaNosova@users.noreply.github.com>
2021-03-19 11:04:11 +0300

    [feat] checking api

---
+++ hseling-web-cat-and-kittens/hseling_web_cat_and_kittens/constants.py
@@ -0,0 +1,7 @@
+UPLOAD_FOLDER = 'student_texts'
+ALLOWED_EXTENSIONS = {'txt'}
+ASPECTS =  [{'id': 'morphology','russian': 'Словоформы, не представленные в CAT'},
+           {'id': 'duplicates','russian': 'Повторы'},
+           {'id': 'genetive_chains', 'russian': 'Цепочки генетивов'},
+           {'id': 'comparatives', 'russian': 'Употребление форм степеней сравнений'},
+           {'id': 'IvsWe', 'russian': 'Последовательное употребление местоимений первого лица'}]
