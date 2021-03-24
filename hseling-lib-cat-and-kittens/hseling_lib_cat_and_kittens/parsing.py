a4f5128ba62cecf2526ce68e6805dde6820e2c78
ElizavetaNosova <58087973+ElizavetaNosova@users.noreply.github.com>
2021-03-25 00:04:25 +0300

    parse model output

--- hseling-lib-cat-and-kittens/hseling_lib_cat_and_kittens/parsing.py
+++ hseling-lib-cat-and-kittens/hseling_lib_cat_and_kittens/parsing.py
@@ -1,12 +1,14 @@
 from ufal.udpipe import Model, Pipeline, ProcessingError, InputFormat
 import os
 import re
+import conllu
 
 def make_conll_with_udpipe(text):
     model_path = os.path.join(os.getcwd(), 'udparsers', 'russian-syntagrus-ud-2.5-191206.udpipe') # здесь указать путь к модели
     model = Model.load(model_path)
     pipeline = Pipeline(model, 'tokenizer=ranges', Pipeline.DEFAULT, Pipeline.DEFAULT, 'conllu')
-    return pipeline.process(text
+    udpipe_output =  pipeline.process(text)
+    return conllu.parse(udpipe_output)
 
 def get_token_start(token: conllu.models.Token):
     return int(token['misc']['TokenRange'].split(':')[0])
