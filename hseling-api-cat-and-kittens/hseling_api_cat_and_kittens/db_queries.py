5d02e2e52847db6bac4fd0da74f758844ba84f08
lauberto <andrea.grillandi@live.it>
2021-03-25 19:25:55 +0300

    add functions to db_queries

--- hseling-api-cat-and-kittens/hseling_api_cat_and_kittens/db_queries.py
+++ hseling-api-cat-and-kittens/hseling_api_cat_and_kittens/db_queries.py
@@ -20,6 +20,18 @@ def get_morph_dict():
 
 def get_values(lemma1, lemma2, morph1, morph2, syntrole, min_, max_):
 
+    if isinstance(lemma1, str):
+        if lemma1 == "":
+            lemma1 = "'^(.*?)$'"
+        else:
+            lemma1 = "'^" + lemma1  + "$'"
+
+    if isinstance(lemma2, str):
+        if lemma2 == "":
+            lemma2 = "'^(.*?)$'"
+        else:
+            lemma2 = "'^" + lemma2 + "$'"
+
     string2syntrole_id = get_syntroles_dict()
     string2morph_id = get_morph_dict()
 
@@ -28,7 +40,7 @@ def get_values(lemma1, lemma2, morph1, morph2, syntrole, min_, max_):
     if morph2 == None:
         morph2 = ""
     
-    if isinstance(morph1, str):
+    if isinstance(morph1, str) and morph1 != "":
         morph1_list = morph1.split(',')
         if morph1_list[0] in string2morph_id.keys():
             pos1 = "'^" + str(string2morph_id[morph1_list[0]]) + "$'"
@@ -40,10 +52,10 @@ def get_values(lemma1, lemma2, morph1, morph2, syntrole, min_, max_):
         else:
             pos1 = "'^[0-9]+$'"
     else:
-        pos1 = ""
+        pos1 = "'^[0-9]+$'"
         morph1 = ""
 
-    if isinstance(morph2, str):
+    if isinstance(morph2, str) and morph2 != "":
         morph2_list = morph2.split(',')
         if morph2_list[0] in string2morph_id.keys():
             pos2 = "'^" + str(string2morph_id[morph2_list[0]]) + "$'"
@@ -55,7 +67,7 @@ def get_values(lemma1, lemma2, morph1, morph2, syntrole, min_, max_):
         else:
             pos2 = "'^[0-9]+$'"
     else:
-        pos2 = ""
+        pos2 = "'^[0-9]+$'"
         morph2 = ""
         
     if isinstance(syntrole, str):
@@ -68,22 +80,30 @@ def get_values(lemma1, lemma2, morph1, morph2, syntrole, min_, max_):
         syntrole = "_"
 
     if isinstance(min_, str):
-        if min_ != "" and min_ > -1 and min_ < 5:
-            min_ = int(min_)
-        elif min_ != "" and min_ > 4:
-            min_ = 4
-        else:
+        if min_ == "":
             min_ = 0
+        else:
+            min_ = int(min_)
+            if min_ > -1 and min_ < 5:
+                min_ = int(min_)
+            elif min_ > 4:
+                min_ = 4
+            else:
+                min_ = 0        
     else:
         min_ = 0
 
     if isinstance(max_, str):
-        if max_ != "" and max_ > 0 and max_ < 6:
-            max_ = int(max_)
-        elif max_ != "" and max_ > 5:
-            max_ = 5
-        else:
+        if max_ == "":
             max_ = 1
+        else:
+            max_ = int(max_)
+            if max_ > 0 and max_ < 6:
+                max_ = int(max_)
+            elif max_ != "" and max_ > 5:
+                max_ = 5
+            else:
+                max_ = 1        
     else:
         max_ = 1 
 
@@ -170,6 +190,82 @@ def bigram_search(search_token, search_metric, search_domain):
     else:
         return ['']
 
+def check_syntrole(result, word, syntrole):
+
+    cur = CONN.cursor()
+    word1_word2_result = []
+
+    if syntrole != "_":
+        for line in result:
+            stmt = """SELECT * FROM wordpairs
+                    WHERE synt_role_id = """ + syntrole + """
+                    AND head_id = """ + word + """
+                    AND dependent_id = """ + line[0] + """
+                    ;"""
+            cur.execute(stmt)
+            res = cur.fetchall()
+            if res:
+                result_dict = {"id_sent" : word["id_sent"], "word1" : word["id_word"], "word2" : line[0]}
+                word1_word2_result.append(result_dict)
+            else:
+                continue
+    else:
+        for line in result:
+            result_dict = {"id_sent" : word["id_sent"], "word1" : word["id_word"], "word2" : line[0]}
+            word1_word2_result.append(result_dict)
+
+    return word1_word2_result
+
+def generate_sent(result):
+
+    cur = CONN.cursor()
+    for result_dict in result:
+        id_sent = str(result_dict["id_sent"])
+        word1 = result_dict["word1"]
+        word2 = result_dict["word2"]
+        sent_start = str(result_dict["word1"] - 10)
+        sent_end = str(result_dict["word2"] + 10)
+        stmt = f"SELECT id_word, word FROM words WHERE id_sent = {id_sent} AND "
+        stmt += f"(id_word >= {sent_start} AND id_word < {sent_end})"
+        stmt += f""
+        cur.execute(stmt)
+        sent = cur.fetchall()
+        sent = ["%%%" + word[1] + "%%%" if word[0] == word1 or word[0] == word2 else word[1] for word in sent]
+        sent = ''.join([('' if c in string.punctuation and c != "(" else ' ')+c for c in sent]).strip()
+        sent = re.sub('^[{}]\s+'.format(string.punctuation), '', sent)
+        sent = re.sub('(?<=\()\s', '', sent)
+    return sent
+
+def get_words4lemma(lemma, morph, pos):
+    cur = CONN.cursor()
+    word_id_list = list()
+
+    stmt = """SELECT tab1.id_unigram FROM (    
+                                SELECT uni.id_unigram, uni.unigram, pos, morph, freq_all, original_cat
+                                FROM unigrams uni
+                                INNER JOIN unitags tags ON uni.id_unigram = tags.id_unigram
+                                WHERE uni.unigram REGEXP {}
+                                AND morph LIKE '%{}%'
+                                ORDER BY freq_all DESC
+                                LIMIT 10
+                            ) AS tab1
+                            INNER JOIN pos pos ON tab1.pos = pos.pos
+                            WHERE pos.id_pos REGEXP {};"""
+    stmt_word = stmt.format(lemma, morph, pos)
+    cur.execute(stmt_word)
+    result = cur.fetchall()
+    for id_unigram in result:
+        sub_stmt = """SELECT words.id_word, words.id_sent
+                FROM unigrams
+                JOIN words
+                ON unigrams.id_unigram = words.id_unigram
+                WHERE unigrams.id_unigram = """ + str(id_unigram[0]) + """
+                LIMIT 3;"""
+        cur.execute(sub_stmt)
+        result = cur.fetchall()
+        word_id_list.extend([{"id_word" : word[0], "id_sent" : word[1]} for word in result])
+    return word_id_list
+
 def single_token_search(search_token):
     """
     search sentences containing input lemma
@@ -222,83 +318,83 @@ def lemma_search(lemma1, lemma2, morph1, morph2, syntrole, min_, max_):
     lemma1, lemma2, pos1, pos2, morph1, morph2, syntrole, min_, max_ = get_values(lemma1, lemma2, morph1, morph2, syntrole, min_, max_)
 
     cur = CONN.cursor()
-    stmt= """SELECT tab2.id_unigram, tab2.pos, tab2.morph, words.id_word, words.id_sent, tab2.freq_all FROM (
-            SELECT tab1.id_unigram, tab1.unigram, tab1.pos, pos.id_pos, morph, freq_all, original_cat FROM (    
-                                SELECT uni.id_unigram, uni.unigram, pos, morph, freq_all, original_cat
-                                FROM unigrams uni
+
+    word1_id_list = get_words4lemma(lemma1, morph1, pos1)
+    word2_id_list = get_words4lemma(lemma2, morph2, pos2)
+    sent_list = list()
+
+    if word1_id_list and word2_id_list and lemma1 != "'^(.*?)$'" and lemma2 != "'^(.*?)$'":
+        for word1 in word1_id_list:
+            id_sent = str(word1["id_sent"])
+            word1_id = word1["id_word"]
+            i = 0
+            for word2 in word2_id_list:
+                if i < 25:
+                    id_sent2 = str(word2["id_sent"])
+                    word2_id = word2["id_word"]
+                    if id_sent == id_sent2 and word2_id >= word1_id + min_ and word2_id <= word1_id + max_:
+                        i += 1
+                        stmt = """SELECT word_tab.id_word, uni.freq_all FROM
+                            (SELECT id_word, id_unigram FROM words WHERE id_word >= {}
+                            AND id_word <= {}
+                            AND id_sent = {}) AS word_tab
+                                INNER JOIN unigrams uni ON word_tab.id_unigram = uni.id_unigram
                                 INNER JOIN unitags tags ON uni.id_unigram = tags.id_unigram
-                                WHERE BINARY uni.unigram = %s
-                                ORDER BY freq_all DESC
-                                LIMIT 2
-                            ) AS tab1
-                            INNER JOIN pos pos ON tab1.pos = pos.pos
-                            WHERE pos.id_pos REGEXP """ + pos1 + """ 
-                            AND morph LIKE '%""" + morph1 + """%'
-                            AND morph <> '_'
-                            ORDER BY freq_all DESC) AS tab2
-                            INNER JOIN words ON tab2.id_unigram = words.id_unigram LIMIT 10;"""
-    cur.execute(stmt, (lemma1, ))
-    result = cur.fetchall()
-    word1_id_list = [{"id_word" : word[3], "id_sent" : word[4]} for word in result] 
-    sent_list = []
-
-    for word1 in word1_id_list:
-        id_sent = str(word1["id_sent"])
-        word1_id = word1["id_word"]
-        stmt = """SELECT word_tab.id_word, uni.freq_all FROM
-                    (SELECT id_word, id_unigram FROM words WHERE id_word >= """ + str(word1_id + min_) + """
-                    AND id_word <= """ + str(word1_id + max_) + """
-                    AND id_sent = """ + id_sent + """) AS word_tab
-                        INNER JOIN unigrams uni ON word_tab.id_unigram = uni.id_unigram
-                        INNER JOIN unitags tags ON uni.id_unigram = tags.id_unigram
-                        INNER JOIN pos pos ON tags.pos = pos.pos
-                    WHERE pos.id_pos REGEXP """ + pos2 + """
-                    AND morph LIKE '%""" + morph1 + """%'
-                    AND morph <> '_' 
-                    ORDER BY uni.freq_all DESC LIMIT 10;"""
-        cur.execute(stmt)
-        result = cur.fetchall()
-        word1_word2_result = []
-
-        if syntrole != "_":
-            for line in result:
-                stmt = """SELECT * FROM wordpairs
-                        WHERE synt_role_id = """ + syntrole + """
-                        AND head_id = """ + word1 + """
-                        AND dependent_id = """ + line[0] + """
-                        ;"""
-                cur.execute(stmt)
-                res = cur.fetchall()
-                if res:
-                    result_dict = {"id_sent" : word1["id_sent"], "word1" : word1["id_word"], "word2" : line[0]}
-                    word1_word2_result.append(result_dict)
+                                INNER JOIN pos pos ON tags.pos = pos.pos
+                            WHERE pos.id_pos REGEXP {}
+                            AND morph LIKE '%{}%'
+                            AND morph <> '_' 
+                            ORDER BY uni.freq_all DESC LIMIT 50;"""
+                        stmt_word1 = stmt.format(str(word1_id + min_), str(word1_id + max_), str(word2_id), id_sent, pos2, morph2)
+                        cur.execute(stmt_word1)
+                        result = cur.fetchall()
+                        word1_word2_result = check_syntrole(result, word1, syntrole)
+                        sent_list.append(generate_sent(word1_word2_result))
                 else:
-                    continue
+                    break
+    
+    elif word1_id_list and lemma2 == "'^(.*?)$'":
+        for word1 in word1_id_list:
+            id_sent = str(word1["id_sent"])
+            word1_id = word1["id_word"]
+            stmt = """SELECT word_tab.id_word, uni.freq_all FROM
+                            (SELECT id_word, id_unigram FROM words WHERE id_word >= {}
+                            AND id_word <= {}
+                            AND id_sent = {}) AS word_tab
+                                INNER JOIN unigrams uni ON word_tab.id_unigram = uni.id_unigram
+                                INNER JOIN unitags tags ON uni.id_unigram = tags.id_unigram
+                                INNER JOIN pos pos ON tags.pos = pos.pos
+                            WHERE morph <> '_' 
+                            ORDER BY uni.freq_all DESC LIMIT 50;"""
+            stmt_word1 = stmt.format(str(word1_id + min_), str(word1_id + max_), id_sent)
+            cur.execute(stmt_word1)
+            result = cur.fetchall()
+            word1_word2_result = check_syntrole(result, word1, syntrole)
+            sent_list.append(generate_sent(word1_word2_result))
+    
+    elif word2_id_list and lemma1 == "'^(.*?)$'":
+        for word2 in word2_id_list:
+            id_sent = str(word2["id_sent"])
+            word2_id = word2["id_word"]
+            stmt = """SELECT word_tab.id_word, uni.freq_all FROM
+                            (SELECT id_word, id_unigram FROM words WHERE id_word >= {}
+                            AND id_word <= {}
+                            AND id_sent = {}) AS word_tab
+                                INNER JOIN unigrams uni ON word_tab.id_unigram = uni.id_unigram
+                                INNER JOIN unitags tags ON uni.id_unigram = tags.id_unigram
+                                INNER JOIN pos pos ON tags.pos = pos.pos
+                            WHERE morph <> '_' 
+                            ORDER BY uni.freq_all DESC LIMIT 50;"""
+            stmt_word1 = stmt.format(str(word2_id - max_), str(word2_id - min_), id_sent)
+            cur.execute(stmt_word1)
+            result = cur.fetchall()
+            word1_word2_result = check_syntrole(result, word1, syntrole)
+            sent_list.append(generate_sent(word1_word2_result))
         else:
-            for line in result:
-                result_dict = {"id_sent" : word1["id_sent"], "word1" : word1["id_word"], "word2" : line[0]}
-                word1_word2_result.append(result_dict)
-        
-        for result_dict in word1_word2_result:
-            id_sent = str(result_dict["id_sent"])
-            word1 = result_dict["word1"]
-            word2 = result_dict["word2"]
-            sent_start = str(result_dict["word1"] - 10)
-            sent_end = str(result_dict["word2"] + 10)
-            stmt = f"SELECT id_word, word FROM words WHERE id_sent = {id_sent} AND "
-            stmt += f"(id_word >= {sent_start} AND id_word < {sent_end})"
-            stmt += f""
-            cur.execute(stmt)
-            sent = cur.fetchall()
-            sent = ["%%%" + word[1] + "%%%" if word[0] == word1 or word[0] == word2 else word[1] for word in sent]
-            sent = ''.join([('' if c in string.punctuation and c != "(" else ' ')+c for c in sent]).strip()
-            sent = re.sub('^[{}]\s+'.format(string.punctuation), '', sent)
-            sent = re.sub('(?<=\()\s', '', sent)
-            sent_list.append(sent)
+            return sent_list
 
     row_headers = ["Example sentence"]
     json_data = []
     for sent in sent_list:
         json_data.append(dict(zip(row_headers, [sent])))
-    
     return json_data
