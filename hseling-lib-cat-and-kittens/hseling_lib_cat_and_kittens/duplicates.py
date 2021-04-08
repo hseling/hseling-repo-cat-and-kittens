bf733d0a97a38c0039668b2029e71270db2fa053
ydnosova <ydnosova@mts.ru>
2021-04-08 20:05:31 +0300

    [fix] spaces

--- hseling-lib-cat-and-kittens/hseling_lib_cat_and_kittens/duplicates.py
+++ hseling-lib-cat-and-kittens/hseling_lib_cat_and_kittens/duplicates.py
@@ -5,37 +5,36 @@ import nltk
 from nltk.tokenize import sent_tokenize
 nltk.download('punkt')
 
-
 def get_windows(l):
-  a = []
-  l_1 = re.split(' ', l)
-  for i in range(0, len(l_1)-3, 1):
-      a.append(' '.join(l_1[i:i+4]))
-  return a
+    a = []
+    l_1 = re.split(' ', l)
+    for i in range(0, len(l_1)-3, 1):
+        a.append(' '.join(l_1[i:i+4]))
+    return a
 
 def find_space(text, beg, end):
-  while (re.match('\s', text[beg-1]) == None) and (beg > 0):
-    beg -= 1
-  while (re.match('\s', text[end-1]) == None) and (end < len(text)):
-    end += 1
-  return beg, end
+    while (re.match('\s', text[beg-1]) == None) and (beg > 0):
+        beg -= 1
+    while (re.match('\s', text[end-1]) == None) and (end < len(text)):
+        end += 1
+    return beg, end-1
 
 def check_duplicates(text):
-  text = re.sub('\s+', ' ', text)
-  words = [word + " " for word in text.split(' ')]
-  sentences = sent_tokenize(text)
-  table = []
-  for i in range(len(sentences)):
-    windows = get_windows(sentences[i])
-    for target in windows:
-      for curr_sent in sentences[i+1:i+4]:
-        pat=r'(?e)((?:%s){e<%i:[а-я]})'
-        pat=pat % (target, int((len(target)+15)/5) - 1)
-        m=regex.search(pat, curr_sent)
-        if m:
-          b = text.rfind(m.group(1))
-          e = text.rfind(m.group(1)) + m.span(0)[1] - m.span(0)[0]
-          right = find_space(text, b, e)
-          table.append({'bos': right[0], 'end': right[1]})
-          table.append({'bos': text.find(target), 'end': text.find(target)+len(target)})
-  return table
+    text = re.sub('\s+', ' ', text)
+    words = [word + " " for word in text.split(' ')]
+    sentences = sent_tokenize(text)
+    table = []
+    for i in range(len(sentences)):
+        windows = get_windows(sentences[i])
+        for target in windows:
+            for curr_sent in sentences[i+1:i+4]:
+                pat=r'(?e)((?:%s){e<%i:[а-я]})'
+                pat=pat % (target, int((len(target)+15)/5) - 1)
+                m=regex.search(pat, curr_sent)
+                if m:
+                    b = text.rfind(m.group(1))
+                    e = text.rfind(m.group(1)) + m.span(0)[1] - m.span(0)[0]
+                    right = find_space(text, b, e)
+                    table.append({'bos': right[0], 'end': right[1]})
+                    table.append({'bos': text.find(target), 'end': text.find(target)+len(target)})
+    return table
--- hseling-web-cat-and-kittens/hseling_web_cat_and_kittens/static/js/highlighting.js
+++ hseling-web-cat-and-kittens/hseling_web_cat_and_kittens/static/js/highlighting.js
@@ -66,11 +66,6 @@ function highlightText(text, problems) {
         return '';
     }
 
-    //If some checking functions included spaces into problem boundary, the space will be unvisible.
-    //To avoid it we will add the spaces before or after highlighted part
-    let partsStartWithSpace = [];
-    let partsEndWithSpace = [];
-
     const id2labels = transformProblemsIntoDictionary(problems);
     const htmlParts2Highlight = [];
     let currentStartId = 0;
@@ -84,56 +79,16 @@ function highlightText(text, problems) {
             htmlParts2Highlight.push(currentHtmlPart);
             currentStartId = currentEndId;
             currentClassLabels = id2labels[currentEndId];
-
-            if (currentTextFragment && currentTextFragment[0] === ' '){
-                partsStartWithSpace.push(currentHtmlPart);
-            }
-            if (currentTextFragment && currentTextFragment[currentTextFragment.length-1] === ' '){
-                partsEndWithSpace.push(currentHtmlPart);
-            }
-
         }
     }
 
     const lastTextFragment = text.slice(currentStartId);
     const lastHtmlPart = addTags(lastTextFragment, currentClassLabels);
     htmlParts2Highlight.push(lastHtmlPart);
-
-    if (lastTextFragment && lastTextFragment[0] === ' '){
-        partsStartWithSpace.push(lastHtmlPart);
-    }
-    if (lastTextFragment && lastTextFragment[lastTextFragment.length-1] === ' '){
-        partsEndWithSpace.push(lastHtmlPart);
-    }
-     let highlitedTextHtml = htmlParts2Highlight.join('');
-
-
-    let alreadyReplacedStarts = [];
-    if (partsStartWithSpace){
-        partsStartWithSpace.forEach(function(part){
-            if (alreadyReplacedStarts.indexOf(part) === -1){
-                const fixedPart = ' ' + part;
-                highlitedTextHtml = highlitedTextHtml.replaceAll(part, fixedPart);
-                alreadyReplacedStarts.push(part);
-            }
-        })
-    }
-
-    let alreadyReplacedEnds = [];
-    if (partsEndWithSpace){
-        partsEndWithSpace.forEach(function(part){
-            if (alreadyReplacedEnds.indexOf(part) === -1){
-                const fixedPart = part + ' ';
-                highlitedTextHtml = highlitedTextHtml.replaceAll(part, fixedPart);
-                alreadyReplacedEnds.push(part);
-            }
-        })
-    }
-
-    //highlitedTextHtml = highlitedTextHtml.replaceAll('[ +]', ' ');
+    
+    let highlitedTextHtml = htmlParts2Highlight.join(' ');
+    highlitedTextHtml = highlitedTextHtml.replaceAll('[ ]+', ' ');
     highlitedTextHtml = highlitedTextHtml.replaceAll('\n', '<br>');
-
-
     return highlitedTextHtml
 }
 
