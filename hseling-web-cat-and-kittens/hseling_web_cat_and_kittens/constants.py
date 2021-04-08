ba8b09ec0875deb5d8c77b59b9176ea0323c8089
ydnosova <ydnosova@mts.ru>
2021-04-08 13:12:22 +0300

    fix text

--- hseling-web-cat-and-kittens/hseling_web_cat_and_kittens/constants.py
+++ hseling-web-cat-and-kittens/hseling_web_cat_and_kittens/constants.py
@@ -4,6 +4,6 @@ ASPECTS =  [
            {'id': 'morphology','russian': 'Словоформы, не представленные в CAT'},
            {'id': 'duplicates','russian': 'Повторы'},
            {'id': 'genetive_chains', 'russian': 'Цепочки генетивов'},
-           {'id': 'comparatives', 'russian': 'Употребление форм степеней сравнений'},
+           {'id': 'comparatives', 'russian': 'Употребление форм степеней сравнения'},
            {'id': 'IvsWe', 'russian': 'Последовательное употребление местоимений первого лица'}
            ]
